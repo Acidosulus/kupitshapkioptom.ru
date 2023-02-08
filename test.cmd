c25b5eff0aa112c14556b4043f6701024b5de166
Acidosulus <acidos@yandex.ru>
2023-02-08 23:54:42 +0700

    2

--- test.cmd
+++ test.cmd
@@ -1,1 +1,1 @@
-python kupitshapkioptom.py good "https://kupitshapkioptom.ru/catalog/detskaya_kollektsiya/detskie-shapki-optom/detskie-sharfy-optom/snud_vorotnik_detskiy_avonde/" "./csvs/test.csv" "1"
+python kupitshapkioptom.py good "https://kupitshapkioptom.ru/catalog/beysbolki-optom/zhenskie/bandany_kosynki_1/bandana_baff_odnotonnyy/" "./csvs/test.csv" "1"
